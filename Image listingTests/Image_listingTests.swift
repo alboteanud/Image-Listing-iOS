d6a7b9769127c0e6092a2bfa1f253ee6dfc8fb18
Dan_Alboteanu <alboteanud@gmail.com>
2020-05-13 21:03:55 +0300

    Initial Commit

---
+++ Image listingTests/Image_listingTests.swift
@@ -0,0 +1,34 @@
+//
+//  Image_listingTests.swift
+//  Image listingTests
+//
+//  Created by Dan Alboteanu on 13/05/2020.
+//  Copyright © 2020 Dan Alboteanu. All rights reserved.
+//
+
+import XCTest
+@testable import Image_listing
+
+class Image_listingTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
